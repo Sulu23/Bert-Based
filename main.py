1e75a70d5174f1bc4bb036b7047096baeaf4a76b
BlackBird237 <40070755+BlackBird237@users.noreply.github.com>
2025-04-01 21:50:35 +0200

    Update main.py
    concaternate tokens en labels aangepast! joepiee

--- main.py
+++ main.py
@@ -1,21 +1,14 @@
-# main.py preprocessing staat er nog in!!
+# main.py
 from model import model_code_switching
 from preprocessing import tokens_to_sentences, concaternate_tokens
 
 # start by opening the data
-with open("lid_spaeng/dev.conll") as data:
+with open("lid_spaeng/debug150.conll") as data:
     #preprocess data
     p_data = tokens_to_sentences(data)
 
 # run the model on the data
 predictions, tokens = model_code_switching(p_data)
 
-for i, val in enumerate(predictions):
-    #if len(val) != (len(tokens[i])-2):
-    print("\n", tokens[i])
-    print(val)
-    print("len labels = ", len(val))
-    print("len tokens = ", len(tokens[i]))
-
-# post + preprocessing for next models
-#concaternate_tokens(tokens, predictions)
+# preprocessing for next models
+predictions, tokens = concaternate_tokens(predictions, tokens)
