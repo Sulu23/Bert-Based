b7ee8902835c759b397f2a3bcf5672cce04c5485
Jessay-Uni <82508154+Jessay-Uni@users.noreply.github.com>
2025-04-02 16:26:22 +0200

    Add files via upload

--- make_tsv.py
+++ make_tsv.py
@@ -1,20 +1,33 @@
-def make_tsv(file):
-    with open('data/tsv_train.conll', 'x') as outfile:
-        for line in file:
+
+def make_tsv(file1, file2):
+    """Transforms data files into true tsv files"""
+
+    with open('data/tsv_train.conll', 'x') as outfile1:
+        for line in file1:
             if line == '\n' or line.startswith('# sent_enum ='):
                 pass
             else:
-                outfile.writelines(line)
+                outfile1.writelines(line)
+
+    with open('data/tsv_dev.conll', 'x') as outfile2:
+        for line in file2:
+            if line == '\n' or line.startswith('# sent_enum = '):
+                pass
+            else:
+                outfile2.writelines(line)
 
 
 def main():
-    filepath = 'data/train.conll'
+    filepath1 = 'data/train.conll'
+    filepath2 = 'data/dev.conll'
 
-    file = open(filepath, 'r')
+    file1 = open(filepath1, 'r')
+    file2 = open(filepath2, 'r')
 
-    make_tsv(file)
+    make_tsv(file1, file2)
 
-    file.close()
+    file1.close()
+    file2.close()
 
 
 if __name__ == '__main__':
