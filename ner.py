5716d07398609c9422d8ada3879eb0b6f6837212
Sgunput <s.d.m.f.gunput@student.rug.nl>
2025-04-02 19:38:17 +0000

    ner.py now replaces the label

--- ner.py
+++ ner.py
@@ -3,18 +3,25 @@
 
 import spacy
 
-def ner(tokens, predictions):
-    # Creates dictionary where the predictions are mapped to the tokens
-    token_label_dict = dict(zip(map(tuple, tokens), map(tuple, predictions)))
-    
-    # Loads spaCy model
-    ml_model = spacy.load("xx_ent_wiki_sm")
-    
-    # Joins all tokens to form one string
+def ner(tokens, labels):       
+    # Joins all tokens to form one string and appends all tokens and all labels into one list. 
     text = " " 
+    token_list = []
+    label_list = []
     for item in tokens:
         tweet = ' '.join(item)
         text = text + tweet + "\n"
+        for word in item:
+            token_list.append(word)
+    for item in predictions:
+        for word in item:
+            label_list.append(word)
+            
+    # Creates dictionary where the labels are mapped to the tokens
+    token_label_dict = dict(zip(token_list, label_list))
+    
+    # Loads spaCy model
+    ml_model = spacy.load("xx_ent_wiki_sm")
     
     # Processes text with spaCy NER model
     processed_text = ml_model(text)
@@ -26,9 +33,11 @@ def ner(tokens, predictions):
     
     # Replaces the label of the token with "ner" if it was found by the NER. 
     for entity in named_entities:
-        for key, value in my_dict.items():
-            if entity in key:
-                my_dict[entity] = "ner"
+            print(entity)
+            if entity in token_label_dict:
+                print("yes")
+                token_label_dict[entity] = 'ner'
+    return token_label_dict
     
     
 
