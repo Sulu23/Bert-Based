8a4560a6ea8c3bd4dc729bcb4e42f2f61357fa47
BlackBird237 <40070755+BlackBird237@users.noreply.github.com>
2025-04-03 22:48:36 +0200

    Update labeler()
    labelt nu ook unkkkk

--- preprocessing.py
+++ preprocessing.py
@@ -3,20 +3,8 @@
 import re
 import string
 
-# this patters checks if string only contains digists or punctuation, copies from
-# https://stackoverflow.com/questions/21696649/filtering-out-strings-that-only-contains-digits-and-or-punctuation-python
-pattern = re.compile("[\d{}]+$".format(re.escape(string.punctuation)))
-  
-
-def normalize(data):
-    # not yet emojis implemented, because the list of emojis is very long, this would slow down the program terribly
-    for list_of_tokens in data:
-        for token in list_of_tokens:
-            # replace links by filler
-
-            pass
-            # replace usernames by filler
-            #re.sub("^@", "CODE_username")
+# This set contains all unk classified tokens from the training data
+unks = {'umm', 'ew', 'u*', '#jdm', 'mhmmm', '6hrs', 'ma', 'hmm', 'bamb', '#r', 'bora', 'hammmmmmm', 'wya', 'nahhh', 'naw', 'goga', 'mhm', 'h52d', 'laɪvz', 'ily', 'uhh', 'yayy', 'ayyye', 'eh', 'sh#+', 'qhfkfieodhwnppvl897jd', 'uu', 'nqv', 'yales', 'dawwwwwhhh', 'cu', '#pinsheswercaslesbis', 'iraaa', 'xoxo', 'foo', 'ajaaaaijaaai', '6-7pm', 'nɪ', 'xxx', 'foh', 'laɪf', 'rr', 'uff', 'meow', '10am', 'pfftt', 'ringo', 'dksishslbehelbeksbdjjdbksbs', 'ricesichuu', 'ia', 'ijuuuuuu', 'nd', 'akdiciwjcoajf', 'ohhh', 'sesh', 'oh', 'eww', 'emm', 'simpa', 'ayyee', 'mm', 'uffff', 'awww', '90s', 'beeeeaaappp', '#s.e.m', 'p2', 'marciiiii', 'wiggy', 'p', 'napearía', 'ehh', 'x', 'frfr', 'imy', 'psh', 'pe', 'pmo', '#allmaton', 'aw', 'ey', 'nismo', 'ughh', 'nawh', 'wow', 'apush', 'zzzaaaaayuuuummmm', 't', 'wjfk', 'em', 'pawsome', 'k', 'p1', 'tl', 'mayneee', 'yay', 'otc', 'l', 'ayyyjuuuesuu', 'kjooo', 'ummm', 'na', 'auuuuuuuuush', 'eeeaahhh', 'awwww', 'ouch', 'bujaja', '#poetuit', 'eaaa', 'hmmm', 'awws', 'jajajajajajajajajajajajaja', 'yahoos', 'pffftt', 'boo', 'jab', 'uh', 'silverado', 'ughhhhhh', 'muah', 'aii', 'tacuachooonnn', 'tssss', 'mlp', '38k', 'ig', 'ghvsdfhjjds', 'ff', 'natin', 'bubu', 'ugh', 'bb', '-spm', 'tsk', 'leɪt', 'kəˈmyu', 'ijuuu', 'svvfsdbhf', 'ouchh', 'j°na', 'bev', 'mmm', 'fənt', 'ggggruto', 'aww', '#freefabo', 'c', 'djfer&djroy', 'og', 'tss', 'iuuuuuu', 'mv', 'awito', 'beffo', 'pip', 'ti', '#mochomista', 'cc', 'awh', 'chi', 'f150', 'b', 'woojuuu', 'n', 'bmo'}
 
 def tokens_to_sentences(data):
     tweets = []
@@ -123,13 +111,22 @@ def labeler(word, labels):
     # input: word and all labels attributed to the word by the model
     # output: predicted label
 
-    # label as 'other' if string only contains punctuation. 
-    if pattern.match(word):
+    # label as 'other' if string contains punctuation, but not hashtags
+    if word.isalpha() == False and word[0] != '#':
         return 'other'
+    
     # All tokens that start with @ are labeled as other
     if word[0]== '@':
         return 'other'
     
+    # check hardcoded list of unk labeled tokens from training data, lowecased, because the set is lowercased as well
+    if word.lower() in unks:
+        return 'unk'
+    
+    # When a character occurs 3 or more times consecutively in a word, we assume it is an unk. 
+    # The regular expression is from: https://stackoverflow.com/questions/6518154/matching-3-or-more-of-the-same-character-in-python/6518192
+    if re.search(r'(\w)\1\1', word):
+        return 'unk'
 
     # map model labels to task labels
     set_labels = set(labels)
@@ -141,6 +138,7 @@ def labeler(word, labels):
         return 'fw'
     else:
         return 'mixed'
+
 
 
 
